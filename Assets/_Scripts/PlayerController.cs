6e34d27e4bb0a64edce8b5f05e06be58210878ce
Arty ytrA <lol.elbells@outlook.com>
2017-01-31 15:42:37 +0100

    initialize

---
+++ Assets/_Scripts/PlayerController.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class Boundary
+{
+    public float xMin, xMax, zMin, zMax;
+}
+
+public class PlayerController : MonoBehaviour {
+
+    private Rigidbody rb;
+    private AudioSource audi;
+    public float speed;
+    public float tilt;
+    public Boundary boundary;
+
+    public GameObject shot;
+    public Transform shotSpawn;    
+
+    // public GameObject projectile;
+    public float fireRate = 0.5F;
+    private float nextFire = 0.0F;
+
+    void Start  ()
+    {
+        rb = GetComponent<Rigidbody>();
+        audi = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {                
+        if (Input.GetButton("Fire1") && Time.time > nextFire)
+        {
+            nextFire = Time.time + fireRate;
+            // GameObject clone = 
+                Instantiate(shot, transform.position, transform.rotation) 
+                // as GameObject
+                ;
+            audi.Play();
+        }
+    }
+
+    void FixedUpdate    ()
+    {
+        float movehorizontal = Input.GetAxis("Horizontal");
+        float movevertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(movehorizontal, 0.0f, movevertical);
+        rb.velocity = movement * speed;
+
+        rb.position = new Vector3
+        (
+            Mathf.Clamp (rb.position.x, boundary.xMin, boundary.xMax),
+            0.0f,
+            Mathf.Clamp(rb.position.z, boundary.zMin, boundary.zMax)
+        );
+
+        rb.rotation = Quaternion.Euler(0.0f, 0.0f, rb.velocity.x * -tilt);
+        
+    }  
+}
